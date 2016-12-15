a2ddecb7be9823da8f0b5bf5a04604729e2f9dcb
Yanch <ongyc1999@gmail.com>
2016-12-15 09:11:33 +0800

    Update custom_calendar.php

--- calendar/custom_calendar.php
+++ calendar/custom_calendar.php
@@ -1,27 +1,25 @@
-$your_google_calendar="https://www.google.com/calendar/embed?src=usa__en@holiday.calendar.google.com&gsessionid=OK";
-$url= parse_url($your_google_calendar);
-$google_domain = $url['scheme'].'://'.$url['host'].dirname($url['path']).'/';
-// Load and parse Google's raw calendar
-$dom = new DOMDocument;
-$dom->loadHTMLfile($your_google_calendar);
-// Change Google's CSS file to use absolute URLs (assumes there's only one element)
-$css = $dom->getElementByTagName('link')->item(0);
-$css_href = $css->getAttributes('href');
-$css->setAttributes('href', $google_domain . $css_href);
-// Change Google's JS file to use absolute URLs
-$scripts = $dom->getElementByTagName('script')->item(0);
-foreach ($scripts as $script) {
-$js_src = $script->getAttributes('src');
-if ($js_src) $script->setAttributes('src', $google_domain . $js_src);
-}
-// Create a link to a new CSS file called custom_calendar.css
-$element = $dom->createElement('link');
-$element->setAttribute('type', 'text/css');
-$element->setAttribute('rel', 'stylesheet');
-$element->setAttribute('href', 'custom_calendar.css');
-// Append this link at the end of the element
-$head = $dom->getElementByTagName('head')->item(0);
-$head->appendChild($element);
-// Export the HTML
-echo $dom->saveHTML();
+<?php
+$calendarpath="https://www.google.com/calendar/embed?src=tdv5lpteblsnat2jpnbiamue90%40group.calendar.google.com&ctz=Asia/Singapore";
+
+//Custom css path
+$newcss="google_calendar.css";
+
+//Iframe contants
+$contents = file_get_contents($calendarpath);
+
+// add secure Google address to root relative links
+$contents = str_replace('<link type="text/css" rel="stylesheet" href="//www.google.com/calendar/', '<link type="text/css" rel="stylesheet" href="https://www.google.com/calendar/', $contents );
+$contents = str_replace('<script type="text/javascript" src="//www.google.com/calendar/', '<script type="text/javascript" src="https://www.google.com/calendar/' , $contents );
+
+//Inject custom css to iframe
+$contents = str_replace('<script>function _onload()', '<script>function _onload()', $contents );
+
+//Set up default view.
+$contents = str_replace('"view":"month"', '"view":"week"', $contents);
+
+//Hide calendar menu
+$contents = str_replace('"showCalendarMenu":true', '"showCalendarMenu":false', $contents);
+
+//Echo calendar
+echo $contents;
 ?>
